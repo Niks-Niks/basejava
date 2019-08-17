60532db41e6e229a1f37ec71b402a3578d62093d
Niks-Niks <44062180+Niks-Niks@users.noreply.github.com>
2019-08-17 12:52:17 +0300

    Hw-7.Create class

---
+++ src/main/model/ListSection.java
@@ -0,0 +1,30 @@
+package main.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class ListSection extends AbstractSection {
+
+    private List<String> list;
+
+    public ListSection(List<String> list) {
+        this.list = list;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListSection that = (ListSection) o;
+        return Objects.equals(list, that.list);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(list);
+    }
+}
