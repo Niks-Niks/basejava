a4b410593f682dbfe6e183c2b84f4bbe51b438a5
Niks-Niks <44062180+Niks-Niks@users.noreply.github.com>
2019-08-17 12:52:36 +0300

    Update class

--- src/main/model/Organization.java
+++ src/main/model/Organization.java
@@ -1,17 +1,36 @@
 package main.model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
-public class Organization{
+public class Organization {
 
     private String title;
-    private LocalDate date;
+    private LocalDate dateStart;
+    private LocalDate dateEnd;
     private String description;
 
-    public Organization(String title, LocalDate date, String description) {
+    public Organization(String title, LocalDate date, LocalDate dateEnd, String description) {
         this.title = title;
-        this.date = date;
+        this.dateStart = date;
+        this.dateEnd = dateEnd;
         this.description = description;
     }
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Organization that = (Organization) o;
+        return Objects.equals(title, that.title) &&
+                Objects.equals(dateStart, that.dateStart) &&
+                Objects.equals(dateEnd, that.dateEnd) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, dateStart, dateEnd, description);
+    }
+
 }
