278237b92fbe5fb6733578c3656ef1fac4484035
Nikita Pozniakov <wingwen.np@gmail.com>
2019-09-21 20:36:55 +0200

    HW-12.

--- src/main/HomeWork.java
+++ src/main/HomeWork.java
@@ -7,21 +7,23 @@ import java.util.stream.Collectors;
 public class HomeWork {
     public static void main(String[] args) {
         int[] array = new int[]{1, 2, 3, 3, 2, 3, 4, 5, 2, 3};
-        Arrays.stream(minValue(array)).forEach(System.out::print);
-        System.out.println("\nNext");
+        System.out.println(minValue(array));
         List<Integer> integer = Arrays.stream(array).boxed().collect(Collectors.toList());
         oddOrEven(integer).stream().forEach(System.out::print);
     }
 
-    private static int[] minValue(int[] values) {
-        int[] arrays = Arrays.stream(values).sorted().distinct().toArray();
+    private static int minValue(int[] values) {
+        int arrays = Arrays.stream(values).distinct().min().getAsInt();
         return arrays;
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
         int sum = integers.stream().mapToInt(Integer::intValue).sum();
+        return getList(integers, sum);
+    }
+
+    private static List<Integer> getList(List<Integer> integers, int sum) {
         List<Integer> result;
-        System.out.println("Sum -> " + sum);
         if (sum % 2 == 0) {
             result = integers.stream().filter(s -> s % 2 != 0).collect(Collectors.toList());
         } else {
