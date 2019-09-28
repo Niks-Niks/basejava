cdc1669a19961d815a69903e438102b882afea00
Niks-Niks <44062180+Niks-Niks@users.noreply.github.com>
2019-09-28 14:58:29 +0200

    HW-13. Realization SQLHelper.

--- src/main/Config.java
+++ src/main/Config.java
@@ -9,7 +9,6 @@ public class Config {
     private static final File PROPS = new File("config\\resumes.properties");
     private static final Config INSTANCE = new Config();
 
-    private static Properties props = new Properties();
     private static File storageDir;
     private static Storage storage;
 
@@ -19,6 +18,7 @@ public class Config {
 
     private Config() {
         try (InputStream is = new FileInputStream(PROPS)) {
+            Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
